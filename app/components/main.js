7614f488a9f26a484c17f1e40706a528a99802cf
Weijian Zeng <weijian@prosparency.com>
2018-11-20 15:01:30 -0500

    dynamic load html for main component

--- app/components/main.js
+++ app/components/main.js
@@ -1,25 +1,31 @@
-
 $(function(){
 
-    Vue.component('page-main', {
-      template: window.App.templates.pages.main,
-      data: function(){
-        return {
-          sponsors: App.sponsors
-        }
-      },
-      mounted: function(){
-         $(".volume.btn").click(
-           function(){
-             var video = document.getElementById("video");
-             video.muted = !video.muted;
-           }
-         );
+    Vue.component('page-main', 
+      function(resolve, reject){
+        $.get("app/components/main.html", function(html){
+          resolve({
+            template:html,
+            data: function(){
+              return {
+                sponsors: App.sponsors
+              }
+            },
+            mounted: function(){
+               $(".volume.btn").click(
+                 function(){
+                   var video = document.getElementById("video");
+                   video.muted = !video.muted;
+                 }
+               );
 
-         Pixlee.init({apiKey:'y7K3EYgGBl9lza5AsEnj'});
-         Pixlee.addSimpleWidget({widgetId:'3570'});
+               Pixlee.init({apiKey:'y7K3EYgGBl9lza5AsEnj'});
+               Pixlee.addSimpleWidget({widgetId:'3570'});
+            }
+          });
 
-      }
+        });
+
+
     });
 
     App.routes.push(
