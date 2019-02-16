060f49773482a43bf86d08f2a36dcd734db78b70
codeofweij <weijian@gmail.com>
2019-02-16 16:39:35 -0500

    new executive files

--- app/sponsors.js
+++ app/sponsors.js
@@ -1,44 +1,43 @@
 $(function(){
   App.sponsors = [
-    { name: 'conEdison',
+    { name: 'Consolidated Edison',
       logo: 'ctn/img/sponsors/conEdison.png',
-      description: 'Con Edison operates one of the world’s largest energy delivery systems. Founded in 1823 as the New York Gas Light company, their electric, gas, and steam service now provides energy for the 10 million people who live in New York City and Westchester County. ConEd is constantly looking toward the future and exploring ways to innovate and take advantage of developing technology. But, more than anything, they’re listening to you and working hard to give you cleaner, more efficient energy choices, and more control over when and how you use your power.',
-
+      description: 'Con Edison operates one of the world’s largest energy delivery systems. Founded in 1823 as the New York Gas Light company, their electric, gas, and steam service now provides energy for the 10 million people who live in New York City and Westchester County. ConEd is constantly looking toward the future and exploring ways to innovate and take advantage of developing technology.',
     },
-    { name: 'BAE',
+    { name: 'BAE Systems',
       logo: 'ctn/img/sponsors/bae.jpg',
       description: 'BAE Systems provides some of the world’s most advanced, technology-led defence, aerospace and security solutions, and employ a skilled workforce of some 83,400 people in over 40 countries. Working with customers and local partners, they develop, engineer, manufacture and support products and systems to deliver military capability, protect national security and people, and keep critical information and infrastructure secure.',
     },
-    { name: 'Magellan',
-      logo: 'ctn/img/sponsors/magellan1.png',
+    { name: 'Magellan Aerospace',
+      logo: 'ctn/img/sponsors/magellan.png',
       description: 'Magellan Aerospace is a global, integrated aerospace company that provides complex assemblies and systems solutions to aircraft and engine manufacturers, and defence and space agencies worldwide. Magellan designs, engineers and manufactures aeroengine and aerostructure assemblies and components for aerospace markets, advanced products for military and space markets, industrial power generation, and specialty products.',
     },
-    { name: 'Titan',
-      logo: 'ctn/img/sponsors/titan.png',
+    { name: 'Titan Machine Corporation',
+      logo: 'ctn/img/sponsors/titanREAL.png',
       description:'Titan Robotics is your Additive Manufacturing solutions provider, helping you implement AM into your production process. From design for AM to material integration and production machines, Titan is your partner from concept to implementation. Titan Robotics manufactures purpose-built Additive Manufacturing machines and with 3D printing experts and a line of Atlas 3D printers in-house, Titan Robotics is fully equipped to manufacture all your custom parts. ',
-    },
-    { name: 'SolidWorks',
-      logo: 'ctn/img/sponsors/solidworks2.png',
-      description: 'Dassault Systèmes SOLIDWORKS Corp. offers complete 3D software tools that let you create, simulate, publish, and manage your data. SOLIDWORKS products are easy to learn and use, and work to help you design better products.',
     },
     { name: 'Queens College',
       logo: 'ctn/img/sponsors/qc.png',
       description: 'Queens College prepares students to become leaders of our global society by offering a rigorous education in the liberal arts and sciences under the guidance of a faculty dedicated to both teaching and research.'
     },
-    { name: 'THAA',
-      logo: 'ctn/img/sponsors/thhs.png',
-      description: 'The Townsend Harris Alumni Association Inc. represents more than 7,000 graduates of Townsend Harris Hall, Townsend Harris High School and Townsend Harris High School at Queens College. The Alumni Association was created to foster, deepen, and enlarge upon friendships begun at Harris to aid and guide present and former students, and to facilitate contacts between Townsend Harris High School and the community.'
+    { name: 'SolidWorks',
+      logo: 'ctn/img/sponsors/solidworks2.png',
+      description: 'Dassault Systèmes SOLIDWORKS Corp. offers complete 3D software tools that let you create, simulate, publish, and manage your data. SOLIDWORKS products are easy to learn and use, and work to help you design better products.',
     },
-    { name: 'Granger',
+    { name: 'Grainger',
       logo: 'ctn/img/sponsors/Grainger.png',
       description: 'Grainger provides over 1.5 million industrial-grade supplies and safety products. For over 90 years, Grainger has built a tradition of serving businesses, big and small, with effortless ways to get the products and services they need. '
     },
+    { name: 'Townsend Harris AA',
+      logo: 'ctn/img/sponsors/thhs.png',
+      description: 'The Townsend Harris Alumni Association represents more than 7,000 graduates of Townsend Harris Hall, Townsend Harris High School and Townsend Harris High School at Queens College. The Alumni Association was created to foster, deepen, and enlarge upon friendships begun at Harris to aid and guide present and former students, and to facilitate contacts between Townsend Harris High School and the community.'
+    },
     { name: 'Mu Family',
-      logo: 'ctn/img/sponsors/mu.png',
+      logo: 'ctn/img/sponsors/mu.jpg',
       description: 'Dr. Harrison T. Mu, M.D. FACS is a Board-certified neurological surgeon and Diplomate of the American Board of Neurological Surgery. He is affiliated with many of the top hospitals in New York City and Long Island areas.'
     },
     { name: 'Werther Family',
-      description: 'Fill in for now'
+      description: ''
     },
 
   ]
