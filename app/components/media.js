46e2ff70a3d656d2dd9284a97be4c2281aab4e2b
codeofweij <weijian@gmail.com>
2019-03-09 13:37:01 -0500

    new exec fies

--- app/components/media.js
+++ app/components/media.js
@@ -1,9 +1,22 @@
+
+
+
 $(function(){
+
+  var pages = [
+  {
+    name:"/ctn/img/marchNLp1.png"
+  },
+  {
+    name:"/ctn/img/marchNLp2.png"
+  },
+  ];
+
   Vue.component('page-media', {
     template:window.App.templates.pages.media,
     data:function(){
       return{
-
+        pages: pages
       }
     },
     mounted:function(){
--- app/components/team.js
+++ app/components/team.js
@@ -4,57 +4,68 @@ $(function(){
 var subteams = [
   { name: 'Mechanical',
      icon: 'ti-settings',
-    description: "We are the subteam responsible for designing and creating a machine set to accomplish a task based on the game that comes out each year. We design and prototype beta robots before moving to the final design. Once we have each element of the robot finalized, we're responsible for putting it together in the most efficient way possible. We have to keep in mind factors such as drop centers, turning radius, height, speed, weight."
+    description: "We are the subteam responsible for designing and creating a machine set to accomplish a task based on the game that comes out each year. We design and prototype beta robots before moving to the final design. Once we have each element of the robot finalized, we're responsible for putting it together in the most efficient way possible. We have to keep in mind factors such as drop centers, turning radius, height, speed, weight.",
+    members: "Coming Soon!"
   },
   {
     name: 'Electrical',
     icon: 'ti-pulse',
-    description: 'Our subteam focuses on wiring circuits and creating the board for our robot. We also work with pneumatics, which involves the use of pressurized air to move mechanisms.'
+    description: 'Our subteam focuses on wiring circuits and creating the board for our robot. We also work with pneumatics, which involves the use of pressurized air to move mechanisms.',
+    members: "Coming Soon!"
   },
   {
     name: '</> Programming',
     icon: '',
-    description: "The Programming Sub-Team is the software side to the team. In programming, we develop the robot code for the autonomous (self-controlled) and teleoperated (driver controlled) periods of the game, vision for the robot so that it is able to detect specific game pieces and landmarks on the field (depending on the year's game), the Scout App which the scouters use to collect and process data, and the team website."
+    description: "The Programming Sub-Team is the software side to the team. In programming, we develop the robot code for the autonomous (self-controlled) and teleoperated (driver controlled) periods of the game, vision for the robot so that it is able to detect specific game pieces and landmarks on the field (depending on the year's game), the Scout App which the scouters use to collect and process data, and the team website.",
+    members: ["Amar Maksumic, Shara Bhuiyan, Hali Huang, Kelvin Yip, Michael Kim, Andy Hoo, Ethan Diep, Johnathan Suero"]
   },
   {
     name: 'Drive Team',
     icon: 'ti-car',
-    description:"Potential drive team members will review the rules and learn different strategies in order to be well equipped for a match. We will learn the different parts of the robot and understand how they work with one another in order to provide feedback on the robots performance. On occasion, we will get to drive the robot. "
+    description:"Potential drive team members will review the rules and learn different strategies in order to be well equipped for a match. We will learn the different parts of the robot and understand how they work with one another in order to provide feedback on the robots performance. On occasion, we will get to drive the robot. ",
+    members: "Coming Soon!"
   },
   {
     name: "Scouting",
     icon: 'ti-eye',
-    description: "We focus on data collection and strategy which is then used during competition. We’ll teach you the ins and outs of last year's game and how to make strategies for it with the assistance of our app. "
+    description: "We focus on data collection and strategy which is then used during competition. We’ll teach you the ins and outs of last year's game and how to make strategies for it with the assistance of our app. ",
+    members: "Coming Soon!"
   },
   {
     name: "Public Relations",
     icon: 'ti-face-smile',
-    description:"Public Relations is the subteam that focuses on developing the brand of the team and making it marketable.\n\nWe will do outreach at public events, where we explain to any interested visitors about our team and what robotics entails. We will do outreach at school, where we will use the power of flyers, social media, and announcements to inform our peers about any robotics events. We will also  help the financial subteam with writing grants, as well as reach out to sponsors with thank-you letters, keeping them updated with the team as well as thanking them for their valuable contributions."
+    description:"Public Relations is the subteam that focuses on developing the brand of the team and making it marketable.\n\nWe will do outreach at public events, where we explain to any interested visitors about our team and what robotics entails. We will do outreach at school, where we will use the power of flyers, social media, and announcements to inform our peers about any robotics events. We will also  help the financial subteam with writing grants, as well as reach out to sponsors with thank-you letters, keeping them updated with the team as well as thanking them for their valuable contributions.",
+    members: "Coming Soon!"
   },
   {
     name: "Finance",
     icon: 'ti-money',
-    description: "The finance department focuses on fundraising which will help all aspects of robotics run at their full capacity.  Join our finance family which encompasses both humanities and STEM so get the best of both worlds when learning about marketing while improving your writing abilities! "
+    description: "The finance department focuses on fundraising which will help all aspects of robotics run at their full capacity.  Join our finance family which encompasses both humanities and STEM so get the best of both worlds when learning about marketing while improving your writing abilities! ",
+    members: "Coming Soon!"
   },
   {
     name: "Media",
     icon: 'ti-video-clapper',
-    description: "In this subteam we document our team in an artistic way. We also work with the Public Relations subteam to chairmans to create the chairmans video. "
+    description: "In this subteam we document our team in an artistic way. We also work with the Public Relations subteam to chairmans to create the chairmans video. ",
+    members: "Coming Soon!"
   },
   {
     name: 'CAD',
     icon: 'ti-ruler-pencil',
-    description: "Our subteam makes dreams become reality. We make 3D models of mechanisms and prototypes. We also make parts to be 3D printed. We further aid the mechanical subteam with the prototyping process."
+    description: "Our subteam makes dreams become reality. We make 3D models of mechanisms and prototypes. We also make parts to be 3D printed. We further aid the mechanical subteam with the prototyping process.",
+    members: "Coming Soon!"
   },
   {
     name: "Animation",
     icon: 'ti-widget',
-    description: "We use Blender, a 3D animation software, to create any animations the team needs. We model, texture, and animate different objects, including characters."
+    description: "We use Blender, a 3D animation software, to create any animations the team needs. We model, texture, and animate different objects, including characters.",
+    members: "Coming Soon!"
   },
   {
     name: "Safety",
     icon: 'ti-heart',
-    description: "We ensure the safety of the team. We keep members safe whether they are in the lab working on the robot or not."
+    description: "We ensure the safety of the team. We keep members safe whether they are in the lab working on the robot or not.",
+    members: "Coming Soon!"
     }
   ]
 
