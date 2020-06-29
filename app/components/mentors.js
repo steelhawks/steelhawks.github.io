24938a3a0e066c29df68d15c2a0f53176f8e54bc
Kelvin Yip <kbrain17@gmail.com>
2020-06-29 00:15:55 -0400

    begin process of changing mentors code

--- app/components/mentors.js
+++ app/components/mentors.js
@@ -1,26 +1,22 @@
 $(function(){
 
-  function mentor(name, title, bio, pic){
-    this.name = name;
-    this.title = title;
-    this.bio = bio;
-    this.pic = pic;
-  }
-
+  //hyperlink for the mentors folder
   var mentorHL = '/ctn/img/mentors/';
+  //hyperlink for a generic profile picture
+  var genericPicture  = "/ctn/img/genericProfile.jpg"
+  //information pertaining to all mentors
   var mentors = [
-    new mentor('Joel Heitman','Head Coach', 'Mr. Heitman is the Head Coach of the Steel Hawks. He joined the Steel Hawks in 2013. His passion for Robotics has allowed the team to soar, and he hopes that with the help of his mentors, he can inspire the next generation of innovators.', "/ctn/img/genericProfile.jpg"),
-    // {
-    //   name: 'Joel Heitman',
-    //   title: 'Head Coach',
-    //   bio: 'Mr. Heitman is the Head Coach of the Steel Hawks. He joined the Steel Hawks in 2013. His passion for Robotics has allowed the team to soar, and he hopes that with the help of his mentors, he can inspire the next generation of innovators.',
-    //   pic: "/ctn/img/genericProfile.jpg"
-    // },
+    {
+      name: 'Joel Heitman',
+      title: 'Head Coach',
+      bio: 'Mr. Heitman is the Head Coach of the Steel Hawks. He joined the Steel Hawks in 2013. His passion for Robotics has allowed the team to soar, and he hopes that with the help of his mentors, he can inspire the next generation of innovators.',
+      pic: mentorHL + "heitman.JPG"
+    },
     {
       name: 'Susan Brustein',
       title: 'Coach',
       bio: 'Ms. Brustein is a Coach for the Steel Hawks. She was one of the founding coaches in 2008. Ms. Brustein is the Assistant Principal of Math, Science, and Technology at our school. She is currently the mentor of our Public Relations subteam.',
-      pic: "/ctn/img/genericProfile.jpg"
+      pic: genericPicture
     },
     {
       name: 'Joshua Raghunath',
@@ -32,49 +28,49 @@ $(function(){
       name: 'David Stern',
       title: 'Coach',
       bio: 'Mr. Stern is a Coach for the Steel Hawks. He joined the Steel Hawks in September of 2016. As a Regents Physics teacher, he helps team members apply what they learn in class to their robot designs.',
-      pic: "/ctn/img/genericProfile.jpg"
+      pic: mentorHL + "stern.JPG"
+    },
+    {
+      name: 'John Tsai',
+      title: 'Coach',
+      bio: 'Mr. Tsai is a coach for the Steel Hawks.',
+      pic: genericPicture
     },
     {
       name: 'Olan-Ray Frith',
       title: 'Mentor',
-      bio: 'Olan is a mechanical mentor for the Steel Hawks. He guides us through the prototyping, designing, and building processes.',
-      pic: "/ctn/img/genericProfile.jpg"
+      bio: 'Olan is the fabrication mentor for the Steel Hawks. He guides us through the prototyping, designing, and building processes.',
+      pic: mentorHL + "olan.JPG"
     },
     {
       name: 'Weijian Zeng',
       title: 'Mentor',
-      bio: 'Weijian is a programming mentor for the Steel Hawks.',
-      pic: "/ctn/img/genericProfile.jpg"
+      bio: 'Weijian is the programming mentor for the Steel Hawks.',
+      pic: mentorHL + "weijian.JPG"
     },
     {
       name: 'Kevin Goetzger',
       title: 'Mentor',
       bio: 'Kevin is a mentor for the Steel Hawks. We are grateful for his work in creating the practice field that we use for Open Field Weekend.',
-      pic: "/ctn/img/genericProfile.jpg"
+      pic: genericPicture
     },
     {
       name: 'Kenneth Leuci',
       title: 'Mentor',
       bio: 'Ken is a mentor for the Steel Hawks. We are grateful for his work in creating the practice field that we use for Open Field Weekend.',
-      pic: "/ctn/img/genericProfile.jpg"
+      pic: genericPicture
     },
     {
-      name: 'John Tsai',
+      name: 'Raj Rao',
       title: 'Mentor',
-      bio: 'Mr. Tsai is a mentor for the Steel Hawks.',
-      pic: "/ctn/img/genericProfile.jpg"
-    },
-    {
-      name: 'Lyndon Wong',
-      title: 'Mentor',
-      bio: 'Lyndon is a mentor for the Steel Hawks.',
-      pic: "/ctn/img/genericProfile.jpg"
+      bio: 'Raj is a mentor for the Steel Hawks.',
+      pic: genericPicture
     },
     {
       name: 'Marcus Barbu',
       title: 'Mentor',
       bio: 'Marcus Barbus is a graduate of the Class of 2016, and was formerly the President of the Steel Hawks. He currently mentors the programming subteam.',
-      pic: "/ctn/img/genericProfile.jpg"
+      pic: genericPicture
     },
     {
       name: 'Ishabul Haque',
@@ -83,10 +79,10 @@ $(function(){
       pic: mentorHL + 'ishabul.JPG'
     },
     {
-      name: 'Sam Diaz',
+      name: 'Alexander Burzynski',
       title: 'Mentor',
-      bio: 'Sam Diaz is a graduate of the Class of 2018, and was formerly the Vice President and Inventory Secretary for the Steel Hawks. He currently mentors the mechanical subteam',
-      pic: "/ctn/img/genericProfile.jpg"
+      bio: 'Alexander is a mentor for the Steel Hawks.',
+      pic: genericPicture
     }
   ];
 
