bb6573a62f61d74e3ea61fd166385c4aea4af33a
Amar Maksumić <amarmaksumich@gmail.com>
2020-07-20 23:17:27 -0400

    updated leadership site

--- app/components/eboard.js
+++ app/components/eboard.js
@@ -10,23 +10,12 @@ $(function(){
   let genericPro = "/ctn/img/genericProfile.jpg"
   let boardHL = "/ctn/img/eboard19/";
   let leaders = [
-    new Leader('Togay Atmaca', 'President', "Hey everyone! My name's Togay and I'm one of the PR Managers for the Steel Hawks. I look for outreach opportunities for the team and help host our own events. I also take part in writing and presenting the Chairman's award, an award given by FIRST that highlights a team's cumulative outreach efforts and impact on their community.", boardHL + "togay.jpeg"),
-    new Leader('James Hopkins', 'Vice-President', "Hi!", genericPro),
-    new Leader('Shara Bhuiyan', 'Vice-President', "Hi!", genericPro),
-    new Leader('Samantha Wong', 'Vice-President', "Hi! My name's Samantha and I'm the team's strategy director. I create strategies for the team during competition using the team's scouting data. My job as the team's strategy director goes hand in hand with my job as the team's lead scout. I'm a junior who really needs to study for my AP classes as much as I study the manual.", boardHL + "samantha.jpeg"),
-    new Leader('Aarti Patel', 'Inventory Secretary', "Hello! My name is Aarti and I am the inventory manager of the team. The work I contribute makes sure that our team runs in an orderly fashion; I help keep track of all the tools we have and where we keep them. If only I applied the same philosophy to my room...", genericPro),
-    new Leader('Neeherika Kotimreddy','Safety Officer','',genericPro),
-    new Leader('Megan Chang','Head of CAD', "Hi, I'm Megan!" ,genericPro),
-    new Leader('Shane Werther','Head of Mechanical', "Hi, I'm Shane!",genericPro),
-    new Leader('Betty Jin','Head of Electrical',"Hi, I'm Betty!",genericPro),
-    new Leader('Raymond Zhuo', 'Head of Photography', "Hi! My name is Raymond and I'm the Director of Photography and Videography. I work to document the team's achievements, failures, happiest, and stressful moments. I also work with the team to create the Chairman's video every year. I'm pretty chill and I will always help my team with whatever they need help with.", boardHL + "raymond.jpeg"),
-    new Leader('Amar Maksumic','Head of Software Engineering',"Hello, soy Amar.",genericPro),
-    new Leader('Sharon Li','Head of Media',"Hello, I'm Sharon!" ,genericPro),
-    new Leader('Amrita Roy', 'Head of Chairmans', "Hi everyone! I'm Amrita and I'm the Head of Chairmans for the Steel Hawks.", boardHL + "amrita.jpeg"),
-    new Leader('Chris Gilson','Head of Finance',"Hi I'm Chris!",genericPro),
-    new Leader('Michael Kim', 'Head Of Scouting', "Hi! I am Michael", genericPro),
-    new Leader('Kelvin Yip', 'Head of Communications', "Hi everyone! My name is Kelvin and I'm the Head of Communications for the Steel Hawks. I update all team members with latest information and oversee the team's Discord Server. I am the lead programmer for the Scout App and Steel Hawks website.",boardHL + "kelvin.jpeg"),
-    new Leader('Clara Graceanu', 'Head of Public Relations', "Hi! I'm Clara!",genericPro)
+    new Leader('Alex Muntean', 'President', "Hi!", genericPro),
+    new Leader('Megan Chang', 'Vice-President', "Hi!", genericPro),
+    new Leader('Amar Maksumić', 'Vice-President of Fabrication and Software Engineering', "Hey! My name is Amar! I overlook the progress that our Fabrication and Software Engineering subteams make to ensure that we are on track to building a functioning robot and a wonderful ScoutApp. I love Bosnian food, but I will never say no to trying some other yummy dishes! Coder at heart!", genericPro),
+    new Leader('Vishnupriya Thummala', 'Vice-President of Communication', "Hi!", genericPro),
+    new Leader('Chris Gilson', 'Vice-President of Finance', "Hi!", genericPro),
+    new Leader('Sharon Li','Vice-President of Public Relations',"Hi!",genericPro)
   ];
   //   {
   //     name: 'Brianna Raghunath',
